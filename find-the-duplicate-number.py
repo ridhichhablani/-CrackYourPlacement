b4f67345de3668468f18784bcb1a2eefb080d41d
ridhichhablani <89528072+ridhichhablani@users.noreply.github.com>
2024-07-16 23:22:36 +0530

    Create find-the-duplicate-number.py

---
+++ find-the-duplicate-number.py
@@ -0,0 +1,10 @@
+#import defaultdict from collections
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        a=set()
+        for i in nums:
+            if i in a:
+                return i
+            a.add(i)
+
+
