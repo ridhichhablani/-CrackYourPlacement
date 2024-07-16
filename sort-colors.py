b3c28814d2cc3f12b922260094da01c938cb361c
ridhichhablani <89528072+ridhichhablani@users.noreply.github.com>
2024-07-16 23:26:05 +0530

    Create sort-colors.py

---
+++ sort-colors.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i]>nums[j]:
+                    nums[i], nums[j]=nums[j], nums[i]
+        return nums
