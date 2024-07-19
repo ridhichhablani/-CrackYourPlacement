ef4a53a7ac56313f4e4edfc25aee360f1ce237b7
ridhichhablani <89528072+ridhichhablani@users.noreply.github.com>
2024-07-19 11:14:56 +0530

    Update two-sum.py

--- two-sum.py
+++ two-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        res=[]
-        for i in range(1,len(nums)):
-            #for j in range(1,len(nums)):
-                if nums[i-1]+nums[i]==target:
-                        res.append(index(i-1))
-                        res.append(index(i))
+        #a=set()
+        for i in range(len(nums)-1):
+            for j in range(1,len(nums)):
+                if (i!=j and nums[i]+nums[j]==target):
+                   return [i,j]
 
-                        
-        return res
+
+
+  
 
         
